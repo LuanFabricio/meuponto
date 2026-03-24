864705299d99750ad0018c7159685145c6891908
Luan Fabricio <luanlimaleite@hotmail.com>
2026-03-24 19:14:22 -0300

    feat: Makes update function option and updates the background color when it is null

--- lib/widgets/button_time2.dart
+++ lib/widgets/button_time2.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 
 class ButtonTime2Widget extends StatelessWidget {
-  const ButtonTime2Widget({super.key, required this.update, required this.text});
+  const ButtonTime2Widget({super.key, required this.text, this.update});
 
-  final VoidCallback update;
+  final Function()? update;
   final String text;
 
   @override
-    Widget build(BuildContext context) {
-      return TextButton(
-        onPressed: update,
-        style: ButtonStyle(
-          shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0),
-              side: BorderSide(color: Colors.black)
-            )
-          )
+  Widget build(BuildContext context) {
+    final WidgetStatePropertyAll<Color>? backgroundColor = update != null
+        ? WidgetStatePropertyAll<Color>(Colors.grey)
+        : null;
+    return TextButton(
+      onPressed: update,
+      style: ButtonStyle(
+        shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: BorderSide(color: Colors.black),
+          ),
         ),
-        child: Text(text),
-      );
-    }
+        backgroundColor: backgroundColor,
+      ),
+      child: Text(text),
+    );
+  }
 }
