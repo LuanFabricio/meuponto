6783783241aa589bda4195b5ea0b358684700d07
Luan Fabricio <luanlimaleite@hotmail.com>
2026-04-10 13:46:05 -0300

    feat(pages/calendar): implements calendar page

---
+++ lib/pages/calendar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meuponto/widgets/calendar.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {});
+        },
+        child: CalendarWidget()
+      ),
+    );
+  }
+}
