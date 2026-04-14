468e128c134269a6e86cec10541df002f0b3271e
Luan Fabrício <luanlimaleite@hotmail.com>
2026-04-13 21:17:07 -0300

    chore: adds flavors for android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,65 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+  id("com.android.application")
+  id("kotlin-android")
+  // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+  id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.meuponto"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+  namespace = "com.example.meuponto"
+  compileSdk = flutter.compileSdkVersion
+  ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_17.toString()
+  }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.meuponto"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+  defaultConfig {
+    // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+    applicationId = "com.meuponto"
+    // You can update the following values to match your application needs.
+    // For more information, see: https://flutter.dev/to/review-gradle-config.
+    minSdk = flutter.minSdkVersion
+    targetSdk = flutter.targetSdkVersion
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+  }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+  buildTypes {
+    release {
+      // TODO: Add your own signing config for the release build.
+      // Signing with the debug keys for now, so `flutter run --release` works.
+      signingConfig = signingConfigs.getByName("debug")
     }
+  }
+
+  flavorDimensions += "default"
+  productFlavors {
+    create("staging") {
+      dimension = "default"
+      resValue(
+        type = "string",
+        name = "app_name",
+        value = "MeuPonto - Staging"
+      )
+      applicationIdSuffix = ".staging"
+    }
+    create("production") {
+      dimension = "default"
+      resValue(
+        type = "string",
+        name = "app_name",
+        value = "MeuPonto"
+      )
+    }
+  }
 }
 
 flutter {
-    source = "../.."
+  source = "../.."
 }
